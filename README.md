66ce8c79fe4928ce9885c423683560cf523ea93d
KB <kb@sunrihtech.com>
2017-09-08 20:53:03 +0530

    Sanity

--- README.md
+++
@@ -1,2 +0,0 @@
-# POC_DS_Stack
-POC_DS_Stack
---
+++ src/main/java/MainStack.java
@@ -0,0 +1,9 @@
+/**
+ * @Author : kalpeshnbhavsar
+ * @Since : 08-09-2017.
+ */
+public class MainStack {
+    public static void main(String []args){
+        System.out.println("Hello World");
+    }
+}
